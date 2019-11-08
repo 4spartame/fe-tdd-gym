16eb79832dec23e998f045fab4b897b691244204
4spartame <4spartame@gmail.com>
2019-11-08 19:01:40 +0900

    test: 예제 테스트 구현 변경

--- src/App.spec.js
+++ src/App.spec.js
@@ -31,54 +31,36 @@ it("getNumber 메서드 호출시 현재 숫자를 반환해야 함", () => {
   expect(number).toBe(1);
 });
 
-it.each([1, 2])("플러스 버튼 %i회 클릭시 값이 증가해야 함", repeat => {
-  // given
-  const app = new App(root, 0);
-  const plus = root.querySelector(".plus");
+it.each([[1, 0], [2, 1]])(
+  "플러스 버튼 %i회 클릭시 값이 증가해야 함(9 -> %i)",
+  (repeat, expected) => {
+    // given
+    const app = new App(root, 9);
+    const plus = root.querySelector(".plus");
 
-  // when
-  for (var i = 0; i < repeat; ++i) {
-    userEvent.click(plus);
+    // when
+    for (var i = 0; i < repeat; ++i) {
+      userEvent.click(plus);
+    }
+
+    // then
+    expect(app.getNumber()).toBe(expected);
   }
+);
 
-  // then
-  expect(app.getNumber()).toBe(repeat);
-});
+it.each([[1, 9], [2, 8]])(
+  "마이너스 버튼 %i회 클릭시 값이 감소해야 함(0 -> %i)",
+  (repeat, expected) => {
+    // given
+    const app = new App(root, 0);
+    const minus = root.querySelector(".minus");
 
-it.each([1, 2])("마이너스 버튼 %i회 클릭시 값이 감소해야 함", repeat => {
-  // given
-  const app = new App(root, 9);
-  const minus = root.querySelector(".minus");
+    // when
+    for (var i = 0; i < repeat; ++i) {
+      userEvent.click(minus);
+    }
 
-  // when
-  for (var i = 0; i < repeat; ++i) {
-    userEvent.click(minus);
+    // then
+    expect(app.getNumber()).toBe(expected);
   }
-
-  // then
-  expect(app.getNumber()).toBe(9 - repeat);
-});
-
-it("값이 9일때 플러스 버튼 클릭시 0이 되어야 함", () => {
-  // given
-  const app = new App(root, 9);
-  const plus = root.querySelector(".plus");
-
-  // when
-  userEvent.click(plus);
-
-  // then
-  expect(app.getNumber()).toBe(0);
-});
-
-it("값이 0일때 마이너스 버튼 클릭시 9가 되어야 함", () => {
-  // given
-  const app = new App(root, 0);
-  const minus = root.querySelector(".minus");
-
-  // when
-  userEvent.click(minus);
-
-  // then
-  expect(app.getNumber()).toBe(9);
-});
+);
