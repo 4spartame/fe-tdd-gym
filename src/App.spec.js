1c883be01fc4a12ea5cadc11767609cb40a58c24
4spartame <4spartame@gmail.com>
2019-11-08 15:32:34 +0900

    test: 특성화 테스트 작성

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,3 +1,53 @@
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test.todo("App.start");
+let root;
+beforeEach(() => {
+  document.body.innerHTML = `<div class="root"></div>`;
+  root = document.querySelector(".root");
+});
+
+it("화면에 카운터 UI를 삽입해야 함", () => {
+  // when
+  App.start(root);
+
+  // then
+  const counter = root.querySelector(".counter");
+  expect(counter).not.toBeNull();
+  expect(counter.querySelector(".plus")).toContainHTML("+");
+  expect(counter.querySelector(".minus")).toContainHTML("-");
+  expect(counter.querySelector(".number")).toContainHTML("0");
+});
+
+describe("버튼 클릭", () => {
+  let plus;
+  let minus;
+  let number;
+  beforeEach(() => {
+    // given
+    App.start(root);
+    plus = root.querySelector(".plus");
+    minus = root.querySelector(".minus");
+    number = root.querySelector(".number");
+  });
+
+  it.each([1, 2])("플러스 버튼 %i회 클릭시 값이 증가해야 함", repeat => {
+    // when
+    for (var i = 0; i < repeat; ++i) {
+      userEvent.click(plus);
+    }
+
+    // then
+    expect(number).toContainHTML(`${repeat}`);
+  });
+
+  it.each([1, 2])("마이너스 버튼 %i회 클릭시 값이 감소해야 함", repeat => {
+    // when
+    for (var i = 0; i < repeat; ++i) {
+      userEvent.click(minus);
+    }
+
+    // then
+    expect(number).toContainHTML(`${0 - repeat}`);
+  });
+});
