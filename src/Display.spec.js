41a455cd4ca6992310b1d677ab35447ca119bb0d
4spartame <4spartame@gmail.com>
2019-11-08 18:18:41 +0900

    feat: 애니메이션 기능 추가

--- src/Display.spec.js
+++ src/Display.spec.js
@@ -10,12 +10,26 @@ describe("Display", () => {
     display = new Display(0);
   });
 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it("mount 호출시 루트 엘리먼트에 .number 요소가 삽입되어야 함", () => {
     // when
     display.mount(root);
 
     // then
-    expect(root.querySelector(".number")).toContainHTML("0");
+    expect(root.querySelector(".number")).not.toBeNull();
+  });
+
+  it(".number 요소 하위에 .before, .after가 삽입되어야 함", () => {
+    // given
+    display.mount(root);
+    const number = root.querySelector(".number");
+
+    // then
+    expect(number.querySelector(".before")).toContainHTML("");
+    expect(number.querySelector(".after")).toContainHTML("0");
   });
 
   it("update 호출시 화면의 숫자가 변경되어야 함", () => {
@@ -27,5 +41,82 @@ describe("Display", () => {
 
     // then
     expect(root.querySelector(".number")).toContainHTML("1");
+  });
+
+  it("update 호출시 변경된 숫자가 .after에 삽입되어야 함", () => {
+    // given
+    display.mount(root);
+
+    // when
+    display.update(1);
+
+    // then
+    expect(root.querySelector(".after")).toContainHTML("1");
+  });
+
+  it("update 호출시 이전 숫자가 .before에 삽입되어야 함", () => {
+    // given
+    display.mount(root);
+    display.update(1);
+
+    // when
+    display.update(2);
+
+    // then
+    expect(root.querySelector(".before")).toContainHTML("1");
+  });
+
+  it("update 호출시 애니메이션 클래스를 .number에 삽입해야 함", () => {
+    // given
+    display.mount(root);
+
+    // when
+    display.update(1, "increase");
+
+    // then
+    expect(root.querySelector(".number")).toHaveClass("increase");
+  });
+
+  describe("애니메이션", () => {
+    let trigger;
+    beforeEach(() => {
+      // given
+      trigger = {};
+      jest
+        .spyOn(HTMLElement.prototype, "addEventListener")
+        .mockImplementation((evName, callback) => {
+          trigger[evName] = callback;
+        });
+      display.mount(root);
+    });
+
+    it("애니메이션 완료 이벤트 발생시 애니메이션 클래스를 .number에서 제거해야 함", () => {
+      // given
+      display.update(1, "increase");
+
+      // when
+      trigger["animationend"]();
+
+      // then
+      expect(root.querySelector(".number")).not.toHaveClass("increase");
+    });
+
+    it("애니메이션 완료 이벤트 발생시 애니메이션 완료 콜백을 제거해야 함", () => {
+      // given
+      jest
+        .spyOn(HTMLElement.prototype, "removeEventListener")
+        .mockImplementation((evName, callback) => {
+          if (trigger[evName] === callback) {
+            trigger[evName] = "removed";
+          }
+        });
+      display.update(1, "increase");
+
+      // when
+      trigger["animationend"]();
+
+      // then
+      expect(trigger["animationend"]).toBe("removed");
+    });
   });
 });
