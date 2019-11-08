48ccdf410f7be699222217603834f20ef59313f9
4spartame <4spartame@gmail.com>
2019-11-08 18:18:41 +0900

    feat: 애니메이션 기능 추가

--- src/Display.js
+++ src/Display.js
@@ -2,14 +2,33 @@ import { createElement } from "./Utils";
 
 export default class Display {
   constructor(defaultValue) {
-    this.el = createElement("div", "number", defaultValue);
+    this.el = createElement("div", "number");
+    this.before = createElement("span", "before");
+    this.after = createElement("span", "after", defaultValue);
+    this.prevValue = defaultValue;
   }
 
   mount(root) {
+    this.el.appendChild(this.before);
+    this.el.appendChild(this.after);
     root.appendChild(this.el);
   }
 
-  update(value) {
-    this.el.innerHTML = value;
+  update(value, animationClass) {
+    this.before.innerHTML = this.prevValue;
+    this.after.innerHTML = value;
+    this.prevValue = value;
+    this.animate(animationClass);
+  }
+
+  animate(animationClass) {
+    this.el.classList.add(animationClass);
+
+    const callback = () => {
+      this.el.classList.remove(animationClass);
+      this.el.removeEventListener("animationend", callback);
+    };
+
+    this.el.addEventListener("animationend", callback);
   }
 }
