4cae0aff94396d8da6eb4ea082c9feea5743300d
4spartame <4spartame@gmail.com>
2019-11-08 16:38:58 +0900

    refactor: app.js 리팩토링

--- src/app.js
+++ src/app.js
@@ -4,21 +4,30 @@ export default class App {
   constructor(root, defaultValue) {
     this.number = defaultValue;
 
-    this.counter = createElement("div", "counter");
-    root.appendChild(this.counter);
+    this._createUI();
+    this._mount(root);
+    this._bindEvent();
+  }
 
-    this.plus = createElement("button", "plus", "+");
-    this.counter.appendChild(this.plus);
-
-    this.minus = createElement("button", "minus", "-");
-    this.counter.appendChild(this.minus);
-
-    this.display = createElement("div", "number", this.number);
-    this.counter.appendChild(this.display);
-
+  _bindEvent() {
     this.plus.addEventListener("click", () => this._increase());
     this.minus.addEventListener("click", () => this._decrease());
   }
+
+  _mount(root) {
+    root.appendChild(this.counter);
+    this.counter.appendChild(this.plus);
+    this.counter.appendChild(this.minus);
+    this.counter.appendChild(this.display);
+  }
+
+  _createUI() {
+    this.counter = createElement("div", "counter");
+    this.plus = createElement("button", "plus", "+");
+    this.minus = createElement("button", "minus", "-");
+    this.display = createElement("div", "number", this.number);
+  }
+
   getNumber() {
     return this.number;
   }
